619832593df5838803e0b03a0b68d0d05895742d
buddyuchiha <Pro100biv@mail.ru>
2025-11-24 22:29:28 +0400

    Keyloack service integrated - auth dependencies to get_current_user - auth config on core

--- src/api/dependencies/auth.py
+++ src/api/dependencies/auth.py
@@ -1,17 +1,19 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from core.auth import keycloak_openid, oauth2_scheme
+import requests
 
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> dict:
-    """
-    Упрощенная проверка через userinfo
-    """
     try:
-        userinfo = keycloak_openid.userinfo(token)
-        print(f"✅ User: {userinfo.get('preferred_username')}")
-        return userinfo
+        token_info = keycloak_openid.introspect(token)
+
+        if not token_info.get('active', False):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token is not active"
+            )
+        
     except Exception as e:
-        print(f"❌ Auth error: {e}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials"
